922b56b93a1a9ebd6b46e7e04f27870b6f00fec8
d3v-tino <zaad1012@outlook.com>
2025-02-18 00:04:26 +0100

    added backend deployment

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
 const config =  {
+    NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
 }
